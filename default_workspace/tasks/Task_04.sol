4eb64d17edfb929b3f56f098cead782a31587481
Kaiki-De-280 <146432626+Kaiki-De-280@users.noreply.github.com>
2025-12-23 13:24:45 +0300

    Add files via upload

---
+++ default_workspace/tasks/Task_04.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+contract Task_04 {
+   mapping(address => uint256) public balances; // Маппинг адресов на балансы
+
+
+   function setBalance(address _addr, uint256 _balance) external {
+       balances[_addr] = _balance; // Установка баланса для указанного адреса
+   }
+
+
+    // Функция, возвращающая баланс по адресу
+   function getBalance(address _addr) external view returns (uint256) {
+       return balances[_addr];
+   }
+
+}
